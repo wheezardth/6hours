99b40ebd7e167f2eca4da0867fe534539830433d
Stefan Kachaunov <wheezardth@users.noreply.github.com>
2019-05-18 19:17:40 +0200

    packages
    only in text due to need for reorganizing everything

---
+++ packages.py
@@ -0,0 +1,3 @@
+# packages in Python are folders which have an __init__.py file in them
+# to access functions in packages, you can either import the entire package and use package.module.function()
+# to resolve to the needed function, or use the from package.module import function
